02bce557dca48b32046b5c15ff7a6ab67f2f6ce9
tkqtang <tkqtang@163.com>
2024-06-07 10:56:36 +0800

    Site updated: 2024-06-07 10:56:34

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/04/14/经验贴/"];function toRandomPost(){
+var posts=["2024/04/14/经验贴/","2024/06/07/GitNote/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
